2daafbac63a3fe310e0eeda21f4d6ebd04076fd6
DavidZemlin <zemlin@gmail.com>
2022-04-08 22:30:55 -0400

    Basic implementation  player movement

---
+++ Assets/Scripts/ItemInteractor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractor : MonoBehaviour
+{
+    [SerializeField] private Player player;
+
+
+
+    private void Awake()
+    {
+        if (player == null)
+        {
+            player = GetComponentInParent<Player>();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I am touching: " + other.gameObject.name);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// script for controllable player characters
+//      it includes movement and interactions
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] [Range(1,2)] private int playerNumber = 1;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    private Vector3 moveInput;
+    private Quaternion toRotation;
+    private float vert;
+    private float hori;
+    private bool moving; // this can be used to signal animation transitions
+
+    private void Awake()
+    {
+        // Initialize characterController
+        if (characterController == null)
+        {
+            characterController = gameObject.GetComponent<CharacterController>();
+        }
+    }
+
+    private void Update()
+    {
+        // reset movement detection
+        moving = false;
+
+        // read inputs for player 1
+        if(playerNumber == 1)
+        {
+            if (Input.GetButton("LeftP1"))
+            {
+                moving = true;
+                hori = -1;
+            }
+            else if (Input.GetButton("RightP1"))
+            {
+                moving = true;
+                hori = 1;
+            }
+            else
+            {
+                hori = 0;
+            }
+
+            if (Input.GetButton("UpP1"))
+            {
+                moving = true;
+                vert = 1;
+            }
+            else if (Input.GetButton("DownP1"))
+            {
+                moving = true;
+                vert = -1;
+            }
+            else
+            {
+                vert = 0;
+            }
+        }
+        else if (playerNumber == 2)
+        {
+            // copy all player one controlls here when done-----------------------------------------------------------------------------------------
+        }
+
+        // set movement vector
+        moveInput = new Vector3(hori * maxSpeed, 0.0f, vert * maxSpeed);
+        characterController.SimpleMove(moveInput);
+
+        // rotate character
+        if (moving)
+        {
+            toRotation = Quaternion.LookRotation(moveInput, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+    }
+}
